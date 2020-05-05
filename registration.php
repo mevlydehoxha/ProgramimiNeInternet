a1a7bcf5eeaca25c4634c0ff6e3a06a87facb455
mevlydehoxha <58596355+mevlydehoxha@users.noreply.github.com>
2020-05-05 16:42:00 +0200

    Commit

--- registration.php
+++ registration.php
@@ -12,10 +12,24 @@ if(isset($_REQUEST['username']))
 {
 	$username=stripslashes($_REQUEST['username']);
 	$username=mysqli_real_escape_string($conn,$username);
+	$firstname=stripslashes($_REQUEST['firstname']);
+	$firstname=mysqli_real_escape_string($conn,$firstname);
+	$lastname=stripslashes($_REQUEST['lastname']);
+	$lasename=mysqli_real_escape_string($conn,$lastname);
+	$email=stripslashes($_REQUEST['email']);
+	$email=mysqli_real_escape_string($conn,$email);
+	$phoneno=stripslashes($_REQUEST['phoneno']);
+	$phoneno=mysqli_real_escape_string($conn,$phoneno);
+	$address=stripslashes($_REQUEST['address']);
+	$adrress=mysqli_real_escape_string($conn,$address);
+	$birthdate=stripslashes($_REQUEST['birthdate']);
+	$birthdate=mysqli_real_escape_string($conn,$birthdate);
+	$shift=stripslashes($_REQUEST['shift']);
+	$shift=mysqli_real_escape_string($conn,$shift);
     $password = stripslashes($_REQUEST['password']);
 	$password=mysqli_real_escape_string($conn,$password);
-	$query="INSERT INTO `users` (username,password)
-	VALUES ('$username','" .md5($password)."')";
+	$query="INSERT INTO `users` (username,firstname,lastname,email,phoneno,address,birthdate,shift,password)
+	VALUES ('$username','$firstname','$lastname','$email','$phoneno','$address','$birthdate','$shift','" .md5($password)."')";
 	$result =mysqli_query($conn,$query);
 	if($result)
 	{
@@ -38,8 +52,16 @@ else{
 	<img src="foto1.jpg" class="gif">
 	<form class="form" action="" method="post">
 	<h1 class="title">REGISTRATION </h1>
+	<input type="text" class="input" name="firstname" placeholder="First name" required/>
+	<input type="text" class="input" name="lastname" placeholder="Last name" required/>
+	<input type="text" class="input" name="email" placeholder="E-mail" required/>
+	<input type="text" class="input" name="phoneno" placeholder="Phone number" required/>
+	<input type="text" class="input" name="address" placeholder="Address" required/>
+	<input type="text" class="input" name="birthdate" placeholder="Birth date" required/>
+	<input type="text" class="input" name="shift" placeholder="Shift" required/>
 	<input type="text" class="input" name="username" placeholder="Username" required/>
 	<input type="password" class="input" name="password" placeholder="Password">
+	<h2>hi</h2>
 	<input type="submit" name="submit" value="Register" class="button">
 	<p class="link"><a href="login.php">CLICK HERE TO LOGIN</a></p>
 	</form>
