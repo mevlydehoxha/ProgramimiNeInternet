c7f3c62450754506bb87eda11ab148a73ae116ea
albionasylaj <58147201+AlbionaSy@users.noreply.github.com>
2020-05-09 19:48:29 +0200

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $conn=mysqli_connect("localhost:3307","root",'',"users");
 if(mysqli_connect_errno())
 {
-	echo "KONEKTIMI ME DATABAZEN TA DESHTUAR".mysqli_connect_error();
+	echo "KONEKTIMI ME DATABAZEN KA DESHTUAR".mysqli_connect_error();
 }
 
 ?>
