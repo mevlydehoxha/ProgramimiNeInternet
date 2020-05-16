463f447f82a61949aebeea034f2d03e9a159fa57
mevlydehoxha <58596355+mevlydehoxha@users.noreply.github.com>
2020-05-16 17:22:01 +0200

    Twitter-api

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	// your app consumer key
+	define( 'CONSUMER_KEY', 'tCShGN23DIzyYqtsVxPJvNbw4' ); 
+
+	// your app consumer secret
+	define( 'CONSUMER_SECRET', 'vBKaQsPxWTOPWwBHFlh0SCoVkqcj3NxLgxrXwVtmvDqikBRP7z' );
+
+	// your app callback url
+	define( 'OAUTH_CALLBACK', 'http://hotelmanagementsystem.gearhostpreview.com/' );
