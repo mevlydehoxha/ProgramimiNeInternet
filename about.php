931a5e3644089ff88e7c09d752f716b1ebf8ba55
albinaramajj <58861175+albinaramajj@users.noreply.github.com>
2020-05-17 16:20:59 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,97 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head lang="en">
+        <meta charset="utf-8">
+        <title>About</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="about.css">
+</head>
+        <hr style="margin: 1em -3.1em 0em -3.1em;">
+        <hr style="position: relative; top:-7px; margin: 0em -3.1em 0em -3.1em; ">
+        <div class="stafi">
+        <h2 style="margin-top: 1em; font-family: Georgia; text-shadow: 2px 2px 5px pruple;"> Stafi </h2>
+        </div>
+        <hr style="margin-top: 2em;">
+        
+        
+        <div class="system">    
+            <div class="flip-card">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+                     <img  class="M" src="Fotot/mevlyde.png" alt="Mevlyde"
+                          style="width:300px;height:270px;">
+            </div>
+            <div class="flip-card-back">
+            <h1>Mevlyde Hoxha</h1> 
+            <p class="ngjyra">Studies Copmuter Engeneering</p>
+            <p class="ngjyra">at University of Prishtina</p> 
+            <p class="ngjyra">~PHP,HTML,CSS,JAVA,C#,PYTHON</p>
+            </div>
+            </div>
+            </div>
+            <div class="flip-card">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+                     <img class="M" src="Fotot/albina1.png"  alt="Albina" style="width:300px;height:270px;">
+            </div>
+            <div class="flip-card-back">
+                <h1>Albin&euml  Ramaj</h1>
+                
+                <p class="ngjyra">Studies Copmuter Engeneering</p>
+                <p class="ngjyra">at University of Prishtina</p>
+                <p class="ngjyra">~PHP,HTML,CSS,JAVA,C#,PYTHON</p>
+            </div>
+            </div>
+            </div>
+        
+            
+            <div class="flip-card">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+                     <img class="M" src="Fotot/albiona.png"
+                          alt="Albiona" style="width:300px;height:270px;" >
+            </div>
+            <div class="flip-card-back">
+            <h1>Albiona Sylaj</h1> 
+            <p class="ngjyra">Studies Copmuter Engeneering</p>
+            <p class="ngjyra">at University of Prishtina</p>
+            <p class="ngjyra">~PHP,HTML,CSS,JAVA,C#,PYTHON</p>
+            </div>
+            </div>
+            </div>
+        </div>
+      
+        <hr style="position:relative; top: 30px;">
+
+        <div id="mainContact">
+            <h3 class="ex">Personal Info</h3>
+            <p class="c">
+                &nbsp;Adresa: Agim Ramadani nr.1 <br>
+                &nbsp;Qyteti: Prishtine<br>
+                &nbsp;Regjioni: 01<br>
+                &nbsp;Shteti:Kosov&euml;<br>
+                &nbsp;Kodi postal:60005 
+            </p>
+           
+        </div>
+        <br>
+        <div id="lastViewed">
+            <h3 class="ex1" >Contact</h3>
+            <p class="c"><a href="#" class="tel" style="color:black">&nbsp;Telefoni:&#128276;+38344864897</a>
+                <br> <a href="#" class="tel" style="color:black">&nbsp;Email:&#128231;hotelmenagement@system.com</a>
+            </p>
+            
+        </div>
+        <br>
+        
+        <footer>
+            <p><a href="#" class="f">Fillimi</a></p>
+             <p>Te drejtat e rezervuara &copy;Fiek2020</p>
+        </footer>   
+        
+    </body>
+
+</html>
