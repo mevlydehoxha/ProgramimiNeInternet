088ab95de220493202db9649a7a7790163751256
albionasylaj <58147201+AlbionaSy@users.noreply.github.com>
2020-05-17 19:45:51 +0200

    Add files via upload

---
+++ head.php
@@ -0,0 +1,31 @@
+ <?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+       <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+ <link rel="stylesheet" type="text/css" media="screen" href="head.css">
+ </head>
+ <body>
+      <div class="faqja">
+        <header> 
+		    <small><a class="a" href="hotelet.php">Home</a></small>
+            <p class="lidhja" style="display: inline"><small>|</small></p>
+           <small><a class="a" href="registration.php">Regjistrohu</a></small>
+            <p class="lidhja" style="display: inline"><small>|</small></p>
+            <small><a class="a" href="login.php">Login</a></small>
+            <p class="lidhja" style="display: inline"><small>|</small></p>
+            <small><a class="a" href="about.php">About</a></small>
+            <p class="lidhja" style="display: inline"><small>|</small></p>
+			
+        </header>
+		<hr style="margin: 1em -3.1em 0em -3.1em;">
+        <hr style="position: relative; top:-7px; margin: 0em -3.1em 0em -3.1em; ">
+          <center>
+          <h2 class="emri">Hotel Menagement System</h2>
+          </center>
+		  
+      </div>
+</body>
